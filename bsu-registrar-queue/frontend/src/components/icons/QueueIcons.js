aeb65ae927ebdbfad3117abdcb9b7a278f840b92
admin <rondominc@gmail.com>
2026-07-24 18:14:47 +0800

    feat(registration): add Adding & Dropping, General Inquiry, and Petition Class icons

--- bsu-registrar-queue/frontend/src/components/icons/QueueIcons.js
+++ bsu-registrar-queue/frontend/src/components/icons/QueueIcons.js
@@ -38,6 +38,18 @@ export const OthersIcon = strokeIcon(
   'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z'
 )
 
+export const AddDropIcon = strokeIcon(
+  'M8 7h12m0 0l-4-4m4 4l-4 4M16 17H4m0 0l4 4m-4-4l4-4'
+)
+
+export const GeneralInquiryIcon = strokeIcon(
+  'M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
+)
+
+export const PetitionIcon = strokeIcon(
+  'M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
+)
+
 const ICONS_BY_TYPE = {
   enrollment: EnrollmentIcon,
   document_request: DocumentIcon,
